b538506c971d2e68861c9afd5810fadb028e7ee3
Avidiyah <121895367+Avidiyah@users.noreply.github.com>
2025-06-20 16:46:51 -0500

    Update __init__.py
    Refactoring for optimization

--- oracle_parser/__init__.py
+++ oracle_parser/__init__.py
@@ -3,3 +3,13 @@
 from .OracleParserPipeline import OracleParserPipeline
 from .EffectParser import EffectParser
 from .EffectPhraseRegistry import STANDARD_EFFECTS, KEYWORD_ABILITIES
+from .OracleParser import OracleParser, EffectIR
+
+__all__ = [
+    "OracleParserPipeline",
+    "EffectParser",
+    "STANDARD_EFFECTS",
+    "KEYWORD_ABILITIES",
+    "OracleParser",
+    "EffectIR",
+]

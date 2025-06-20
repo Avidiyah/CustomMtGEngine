a702243d1d075c5942f12c231e19aa4626ccbf70
Avidiyah <121895367+Avidiyah@users.noreply.github.com>
2025-06-20 18:49:43 -0500

    Update __init__.py

--- effect_execution/__init__.py
+++ effect_execution/__init__.py
@@ -4,4 +4,4 @@ from .effect_engine import EffectEngine, EffectContext, DynamicReferenceManager
 from .StaticChecker import StaticChecker
 from .StaticEffectDescriptor import StaticEffectDescriptor
 from .ReplacementEffectManager import ReplacementEffectManager
-from .TargetingSystem import TargetingSystem
+

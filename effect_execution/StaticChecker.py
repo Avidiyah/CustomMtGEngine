2435b2fbbca0cd31d61ec61de892a53ce9ea63c1
Avidiyah <121895367+Avidiyah@users.noreply.github.com>
2025-06-21 17:08:23 -0500

    Update StaticChecker.py

--- effect_execution/StaticChecker.py
+++ effect_execution/StaticChecker.py
@@ -1,9 +1,19 @@
 # === StaticChecker ===
 # from oracle_parser.OracleRulesLayer import LayerManager  # TODO: implement rule engine
 
+
+class _StubLayerManager:
+    """Temporary stand-in until the full LayerManager exists."""
+
+    def register_effect(self, _effect):
+        pass
+
+    def apply_layers(self, _game_state):
+        pass
+
 class StaticChecker:
     def __init__(self):
-        # self.layer_manager = LayerManager()
+        self.layer_manager = _StubLayerManager()
         pass
 
     def register(self, effect):

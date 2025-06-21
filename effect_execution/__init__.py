497b7bc0c37ee4f9a0d2667cd84db8f34f0f9661
Avidiyah <121895367+Avidiyah@users.noreply.github.com>
2025-06-20 19:50:36 -0500

    Update __init__.py

--- effect_execution/__init__.py
+++ effect_execution/__init__.py
@@ -1,4 +1,4 @@
 # === effect_execution Anchor Module ===
 
-from .effect_engine import EffectEngine, EffectContext, DynamicReferenceManager
+from .EffectEngine import EffectEngine, EffectContext, DynamicReferenceManager
 from .StaticChecker import StaticChecker

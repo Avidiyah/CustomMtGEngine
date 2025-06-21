6cc7ff26b7fd9171d2240ebdbcd43718e7ceb90c
Avidiyah <121895367+Avidiyah@users.noreply.github.com>
2025-06-20 19:48:42 -0500

    Update StaticChecker.py

--- effect_execution/StaticChecker.py
+++ effect_execution/StaticChecker.py
@@ -1,9 +1,10 @@
 # === StaticChecker ===
-from oracle_parser.OracleRulesLayer import LayerManager
+# from oracle_parser.OracleRulesLayer import LayerManager  # TODO: implement rule engine
 
 class StaticChecker:
     def __init__(self):
-        self.layer_manager = LayerManager()
+        # self.layer_manager = LayerManager()
+        pass
 
     def register(self, effect):
         """Register a StaticEffectDescriptor or similar into the LayerManager."""
